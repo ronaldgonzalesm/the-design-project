c587990520f539456026522a881b64412350ff4e
Ronald Gonzales <ronaldgonzalesm95@gmail.com>
2023-10-03 18:13:39 +0200

    Maquetacion menu mobile

---
+++ build/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const d = document;
+
+/** Menu responsive */
+let $btnHamburger = d.querySelectorAll('.hamburger');
+let $navigation = d.querySelector('.navigation');
+
+$btnHamburger.forEach((btn) => {
+    btn.addEventListener("click", e => {
+        $navigation.classList.toggle("navigation--active");
+    })
+});
